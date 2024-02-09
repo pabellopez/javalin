7207c4ddf22fe2c70f2e5663beaca967a38b1905
Pabel Lopez <45464452+pabellopez@users.noreply.github.com>
2024-02-09 17:03:14 -0400

    dev

---
+++ src/main/java/org/example/H2JDBCUtils.java
@@ -0,0 +1,115 @@
+package org.example;
+
+import java.sql.*;
+
+public class H2JDBCUtils {
+    private static String jdbcURL = "jdbc:h2:mem:test";
+//    private static String jdbcURL = "jdbc:h2:~/test";
+    private static String jdbcUsername = "sa";
+    private static String jdbcPassword = "";
+
+    static Connection connection = null;
+
+    static void getConnection() {
+        try {
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void init() {
+        getConnection();
+        try {
+            String sql = "";
+            Statement statement = connection.createStatement();
+            sql = "create table usuario(" +
+                    "  id  int primary key auto_increment," +
+                    "  username varchar(20)," +
+                    "  nombre varchar(20)," +
+                    "  password varchar(20)," +
+                    "  administrator boolean," +
+                    "  autor boolean" +
+                    "  );";
+
+            sql+= "create table articulo(" +
+                    "  id  int primary key auto_increment," +
+                    "  titulo varchar(50)," +
+                    "  cuerpo varchar(5000)," +
+                    "  autor int," +
+                    "  fecha date" +
+                    "  );";
+
+            sql+= "create table etiqueta(" +
+                    "  id  int primary key auto_increment," +
+                    "  etiqueta varchar(50)" +
+                    "  );";
+
+
+
+            sql += "create table comentario(" +
+                    "  id  int primary key auto_increment," +
+                    "  username varchar(5000)," +
+                    "  autor int," +
+                    "  articulo int" +
+                    "  );";
+            statement.execute(sql);
+            insertRecord();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    static void insertRecord() throws SQLException {
+
+        String INSERT_USERS_SQL = "INSERT INTO usuario" +
+                "  (id, username, nombre, password, administrator,autor) VALUES " +
+                " (?, ?, ?, ?, ?,?);";
+        // Step 1: Establishing a Connection
+        try {
+
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, "admin");
+            preparedStatement.setString(3, "ADMINISTRADOR");
+            preparedStatement.setString(4, "admin");
+            preparedStatement.setBoolean(5, true);
+            preparedStatement.setBoolean(6, false);
+
+            // Step 3: Execute the query or update query
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            // print SQL exception information
+            e.printStackTrace();
+        }
+
+        // Step 4: try-with-resource statement will auto close the connection.
+    }
+
+
+    public static ResultSet getQuery(String sql) {
+        ResultSet rs = null;
+        try {
+            // Step 2:Create a statement using connection object
+            Statement statement = connection.createStatement();
+            rs = statement.executeQuery(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public static void execute(String sql) {
+        try {
+            // Step 2:Create a statement using connection object
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,101 @@
+package org.example;
+
+import io.javalin.Javalin;
+import io.javalin.config.JavalinConfig;
+import io.javalin.http.HttpStatus;
+import io.javalin.http.UnauthorizedResponse;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import org.h2.tools.Server;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+public class Main {
+
+    static void aplicarConfiguracion(JavalinConfig config) {
+        //AQUI CONGIGURAMOS QUE EL PROYECTO EN VEZ DE INICIAR EN http://localhost:7071/ inicie en http://localhost:7071/my-app ASI TENEMOS UNA IDENTIDAD A NUESTRA APP
+        config.router.contextPath = "/my-app";
+        config.staticFiles.add("static/", Location.CLASSPATH);
+
+
+        //AQUI APLICAMOS LA CONFIGURACION PARA QUE JAVALIN ENTIENDA QUE SU MANEJADOR DE PLATILLA SERA THYMELEAF Y QUE SU RUTA ES EN resources/templates Y QUE SUS ARCHIVOS .html
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/"); // Carpeta donde se encuentran las plantillas
+        templateResolver.setSuffix(".html"); // Extensión de archivo
+        templateResolver.setTemplateMode("HTML"); // Modo de plantilla (HTML5)
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        config.fileRenderer(new JavalinThymeleaf(templateEngine));
+
+
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+
+//        Server server = Server.createTcpServer(args).start();
+        Server server = Server.createWebServer(args).start();
+        H2JDBCUtils.init();
+        Javalin app = Javalin.create(config -> {
+                    aplicarConfiguracion(config);
+
+                }).get("/", ctx -> {
+                    Map<String, Object> model = new HashMap<>();
+                    model.put("titulo", "Javalin app");
+                    model.put("nombre", "Justin Quijada");
+                    ctx.render("index.html", model);
+                })
+                .start(7071);
+
+
+//        app.before(context -> {
+//            System.out.println(context.fullUrl());
+//            if (context.sessionAttribute("usuario") == null && !context.fullUrl().contains("login") && !context.fullUrl().contains("assets")) {
+//                context.redirect("./login");
+//            }else {
+//                if (context.fullUrl().contains("login")){
+//                    context.redirect("./home");
+//                }
+//            }
+//        });
+
+        app.get("/login", context -> {
+            context.render("login.html");
+        });
+        app.post("/login/iniciar", context -> {
+
+            String sql = "select * from usuario where username = ? and password = ?";
+            PreparedStatement preparedStatement = H2JDBCUtils.connection.prepareStatement(sql);
+            preparedStatement.setString(1, context.formParam("usuario"));
+            preparedStatement.setString(2, context.formParam("password"));
+            ResultSet rs = preparedStatement.executeQuery();
+            Map<String, Object> usuario = null;
+
+            while (rs.next()) {
+                usuario = new HashMap<>();
+                usuario.put("username", rs.getString("username"));
+                usuario.put("nombre", rs.getString("nombre"));
+                usuario.put("administrator", rs.getBoolean("administrator"));
+                usuario.put("autor", rs.getBoolean("autor"));
+
+            }
+            if (usuario != null) {
+                context.sessionAttribute("usuario", usuario);
+                context.result("true");
+            } else {
+                context.result("false");
+            }
+        });
+
+        app.get("/home", context -> {
+            context.render("home.html");
+        });
+    }
+}
+
